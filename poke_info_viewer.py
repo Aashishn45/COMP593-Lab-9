d4095d4d7ea54d29c4de411384bac29c576f1cbf
Aashish Neupane <156460188+Aashishn45@users.noreply.github.com>
2024-03-26 10:27:42 -0400

    created the frames

--- poke_info_viewer.py
+++ poke_info_viewer.py
@@ -6,16 +6,32 @@ Description:
 Usage:
   python poke_info_viewer.py
 """
-from tkinter import *
-from tkinter import ttk
+
+from tkinter import ttk, Tk
 
 # Create the main window
 root = Tk()
 root.title("Pokemon Information")
 
 # TODO: Create the frames
+inp = ttk.Frame(root)
+inp.grid(row=0, column=0)
+
+inf = ttk.LabelFrame(root, text="Info")
+inf.grid(row=1, column=0)
+
+stat = ttk.LabelFrame(root, text="Stats")
+stat.grid(row=1, column=1)
+
+
 
 # TODO: Populate the user input frame with widgets
+inp_labl = ttk.Label(inp, text="Pokemon Name:")
+inp_labl.grid(row=0, column=0)
+
+
+
+
 
 # TODO: Define button click event handler function
 
